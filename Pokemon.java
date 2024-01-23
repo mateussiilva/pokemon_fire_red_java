bbfd838031825c6a08f7d1ba307a412877285565
mateussiilva <mateuspython3010@gmail.com>
2024-01-22 21:11:29 -0300

    Primeiro Commit

---
+++ Pokemon.java
@@ -0,0 +1,10 @@
+
+class Pokemon {
+    String nome;
+    float lifePoints = 100f;
+
+    public Pokemon(String nome, float lifePoints) {
+        this.nome = nome;
+
+    };
+};
---
+++ game.java
@@ -0,0 +1,9 @@
+
+// import Pokemon;
+
+public class game {
+    public static void main(String[] args) {
+        Pokemon pokemon = new Pokemon("Mateus", 0);
+        System.out.println(pokemon);
+    }
+}
